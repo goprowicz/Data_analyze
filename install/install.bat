f4bd539df91e7422e4f5e3aa16aa0a5ddb609fd0
Mateusz Wojtowicz <matelusz.wojtowicz@gmail.com>
2023-10-16 19:09:06 +0200

    uploading

---
+++ install/install.bat
@@ -0,0 +1,47 @@
+@echo off
+:start
+cls
+
+python-3.11.4-amd64
+echo "installed - python 3.11"
+
+
+pip3 install tk
+pip install tk
+echo "installed - tkinter"
+
+pip3 install pandas
+pip install pandas
+echo "installed - pandas"
+
+pip3 install numpy
+pip install numpy
+echo "installed numpy"
+
+pip3 install openpyxl
+pip install openpyxl
+echo "installed openpyxl"
+
+pip3 install xlrd
+pip install xlrd
+echo "installed xlrd"
+
+pip3 install xlwt
+pip install xlwt
+echo "installed xlwt"
+
+pip3 install xlsxwriter
+pip install xlsxwriter
+echo "installed xlsxwriter"
+
+pip3 install matplotlib
+pip install matplotlib
+echo "installed matplotlib"
+
+pip3 install openpyxl
+pip install openpyxl
+echo "installed openpyxl"
+
+
+pause
+exit
